eb42267d624a2a02cd87f57efa45fbc41c89ce28
UltraGnome <ultragnomeultra@duck.com>
2024-12-18 18:04:03 -0700

    adding two pointers 2/3 solved

--- TwoPointers/leetcode11.go
+++ TwoPointers/leetcode11.go
@@ -5,13 +5,50 @@ import (
 )
 
 func maxArea(height []int) int {
-	result := 0
+	maxWater := 0
 
-	return result
+	p1, p2 := 0, len(height)-1
+
+	for p1 < p2 {
+		var minHeight int
+		if height[p1] > height[p2] {
+			minHeight = height[p2]
+			p2--
+		} else {
+			minHeight = height[p1]
+			p1++
+		}
+
+		thisWater := minHeight * (p2 - p1 + 1)
+
+		if thisWater > maxWater {
+			maxWater = thisWater
+		}
+
+	}
+
+	return maxWater
 
 }
 
 func main() {
-	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})) //49
+	fmt.Println(maxArea([]int{1, 1}))                      //1
+	fmt.Println(maxArea([]int{1, 2, 1}))                   //2
+	fmt.Println(maxArea([]int{1, 2, 4, 3}))                //4
+	fmt.Println(maxArea([]int{8, 7, 2, 1}))                //4
 
 }
+
+//You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+//Find two lines that together with the x-axis form a container, such that the container contains the most water.
+//Return the maximum amount of water a container can store.
+//Notice that you may not slant the container.
+//Example 1:
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+//Example 2:
+//
+//Input: height = [1,1]
+//Output: 1
--- TwoPointers/leetcode125.go
+++ TwoPointers/leetcode125.go
@@ -2,16 +2,56 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
 
-func maxArea(height []int) int {
-	result := 0
+func isPalindrome(s string) bool {
+	result := true
+	s = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(s, "")
+	s = strings.ToLower(s)
+	s = strings.ReplaceAll(s, " ", "")
+
+	if len(s) == 0 {
+		return result
+	}
+
+	letters := strings.Split(s, "")
+
+	p1, p2 := 0, len(s)-1
+	for p1 <= p2 {
+		if letters[p1] != letters[p2] {
+			result = false
+			return result
+		}
+		p1++
+		p2--
+	}
 
 	return result
-
 }
 
 func main() {
-	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(isPalindrome("race a car"))
+	fmt.Println(isPalindrome(" "))
+	fmt.Println(isPalindrome("aa"))
 
 }
+
+//Example 1:
+//
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+//Example 2:
+//
+//Input: s = "race a car"
+//Output: false
+//Explanation: "raceacar" is not a palindrome.
+//Example 3:
+//
+//Input: s = " "
+//Output: true
+//Explanation: s is an empty string "" after removing non-alphanumeric characters.
+//Since an empty string reads the same forward and backward, it is a palindrome.
